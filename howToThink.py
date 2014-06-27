eb1f4303e4f6023327245f3b7908f805c1754244
Robin Malhotra <malhotra.robin@yahoo.co.in>
2014-06-28 00:52:15 +0530

    chapter 11 completed

--- howToThink.py
+++ howToThink.py
@@ -1,7 +1,10 @@
-def dotMultiplication(l1,l2):
-	l3=0
-	for i in range(0,len(l1)):
-		l3=l3+l1[i]*l2[i]
-	return l3
-print dotMultiplication([1, 2], [1, 4])
-	
+def swap(x, y):      # Incorrect version
+     print("before swap statement: x:", x, "y:", y)
+     (x, y) = (y, x)
+     print("after swap statement: x:", x, "y:", y)
+
+a = ["This", "is", "fun"]
+b = [2,3,4]
+print("before swap function call: a:", a, "b:", b)
+swap(a, b)
+print("after swap function call: a:", a, "b:", b)
